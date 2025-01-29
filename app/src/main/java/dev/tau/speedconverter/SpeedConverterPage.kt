6232777aa75934f88ed5471de344adea6dca41ab
tauimonen <tauimonen@gmail.com>
2025-01-29 19:34:57 +0200

    Create a TextField for the input speed

--- app/src/main/java/dev/tau/speedconverter/SpeedConverterPage.kt
+++ app/src/main/java/dev/tau/speedconverter/SpeedConverterPage.kt
@@ -1,9 +1,11 @@
 package dev.tau.speedconverter
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,7 +20,14 @@ fun SpeedConverterPage(modifier: Modifier = Modifier) {
                 title = { Text(text = "Speed Converter") }
             )
         }
-    )
+    ) {
+        Column(
+            modifier = modifier.padding(it)
+        ) {
+            TextField(value = "", onValueChange = {}, label = { Text("Speed in km/h") })
+        },
+        modifier
+    }
 }
 
 @Preview
