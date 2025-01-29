b49eb3fb6db22f8bf6cedc267af59e8dc106865e
tauimonen <tauimonen@gmail.com>
2025-01-29 20:14:44 +0200

    Fix the build problem

--- app/src/main/java/dev/tau/speedconverter/SpeedConverterPage.kt
+++ app/src/main/java/dev/tau/speedconverter/SpeedConverterPage.kt
@@ -1,6 +1,7 @@
 package dev.tau.speedconverter
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -10,6 +11,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -24,9 +26,14 @@ fun SpeedConverterPage(modifier: Modifier = Modifier) {
         Column(
             modifier = modifier.padding(it)
         ) {
-            TextField(value = "", onValueChange = {}, label = { Text("Speed in km/h") })
-        },
-        modifier
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text("Speed in km/h")
+                },
+                modifier = Modifier.fillMaxWidth().padding(32.dp))
+        }
     }
 }
 
